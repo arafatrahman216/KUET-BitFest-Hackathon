c8b8b368a47b0c417546bb7690ef1c11e4e439d7
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2024-12-21 21:01:03 +0600

    db connect success

---
+++ Backend/db/db.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+
+// Configure the PostgreSQL connection
+const client = new Client({
+  user: 'postgres',        // Database username
+  host: 'localhost',            // Database host
+  database: 'postgres',    // Database name
+  password: 'arafat219',    // Database password
+  port: 5432,                   // Default PostgreSQL port
+});
+
+// Connect to the database
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Connection error', err.stack));
+
+// Export the client for reuse
+module.exports = client;
--- Backend/index.js
+++ Backend/index.js
@@ -13,10 +13,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+const client = require('./db/db.js');
+
+const getRecipes = async () => {
+  try {
+    const res = await client.query('SELECT * FROM Recipes');
+    return res.rows;
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+    return [];
+  }
+};
+
+
+
 
 app.get('/', (req, res) => {
+    getRecipes();
     res.send('Hello, Express!');
 });
+
+app.get('/recipes', async (req, res) => {
+    const recipes = await getRecipes();
+    res.json(recipes);
+
+});
+
+
 
 // Start the Server
 app.listen(PORT, () => {
